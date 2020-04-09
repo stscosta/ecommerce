d7a1fee5c6eff9e540cde0672799720389f56378
stscosta <civadm135@gmail.com>
2020-04-09 10:31:45 -0300

    Criação da classe PageAdmin

--- index.php
+++ index.php
@@ -4,15 +4,24 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim; // CLasses que serão carregadas 
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();  // criação de rotas 
 $app->config('debug', true);
+
+//ROTA DAS PÁGINAS GERAIS
 $app->get('/', function() {
-    
 	$page = new Page();
 	$page->setTpl("index"); 
-
 });
+
+// ROTA DAS PÁGINAS DO ADMINISTRADOR
+$app->get('/admin', function() {
+	$page = new PageAdmin();
+	$page->setTpl("index"); 
+});
+
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -25,13 +25,13 @@ class Page{
 
 
 	//GERA O CABEÇALHO DA PÁGINA
-	public function __construct($opts = array()){ 
+	public function __construct($opts = array(), $tpl_dir = "/views/"){ 
  
  		$this->options = array_merge($this->defaults, $opts);
 
  		$config = array(
 		    "base_url"      => null,
-		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
+		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
 		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 		    "debug"         => false
 		);
