02a034c319f4f658e6b2ff62747af671e1bfb508
stscosta <civadm135@gmail.com>
2020-04-29 11:15:40 -0300

    Minha conta

---
+++ views-cache/categories-menu.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/11">Eletrodomésticos</a></li><li><a href="/categories/10">Eletrônicos</a></li><li><a href="/categories/12">Esporte e Aventura</a></li>
